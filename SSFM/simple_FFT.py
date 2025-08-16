68f036ff01ac520d341831273cc8eacbca6280c8
Gautam K Saharia <95095696+gautamksaharia@users.noreply.github.com>
2025-08-16 12:24:34 +0530

    Update simple_FFT.py

--- SSFM/simple_FFT.py
+++ SSFM/simple_FFT.py
@@ -28,3 +28,44 @@ plt.plot( k[:N//2], np.abs(yf)[:N//2])
 plt.xlabel("frequency")
 plt.ylabel("amplitude")
 plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+# below code uses rfft which is for real input 
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.fft import fft, fftfreq, fftshift, rfft, rfftfreq
+
+N =100
+t = np.linspace(0, 5, N)
+dt = t[1] - t[0]
+y0 = np.sin(2*np.pi*t) # sin(2 pi f t) f is the frequency 1
+y1 =  np.sin(2*np.pi*5*t) # frequency 5
+y2 =  np.sin(2*np.pi*8*t) # frequency 8
+y = y0 + y1 + y2
+plt.plot(t, y)
+plt.xlabel("time")
+plt.ylabel("y value")
+plt.show()
+
+
+
+yf = rfft(y)/N
+k = rfftfreq(N, dt)
+plt.plot( k, np.abs(yf))
+plt.xlabel("frequency")
+plt.ylabel("amplitude")
+plt.show()
+
+
+
+
